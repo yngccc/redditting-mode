d079e5b81bb6dc9f6ea3f96fc4bf0a6d7c491e95
yngccc@gmail.com <yngccc@gmail.com>
2014-03-20 00:47:49 -0400

    add buffer

--- redditting-mode.el
+++ redditting-mode.el
@@ -9,10 +9,6 @@
 (require 'url)
 (require 'json)
 
-(defconst redditting-url "http://www.reddit.com")
-(defconst redditting-account-info-url (concat redditting-url "/api/me.json"))
-(defconst redditting-login-url (concat redditting-url "/api/login"))
-
 (defconst redditting-listing-kind "Listing")
 (defconst redditting-more-kind "more")
 (defconst redditting-comment-kind "t1")
@@ -23,44 +19,69 @@
 (defconst redditting-award-kind "t6")
 (defconst redditting-promo-kind "t8")
 
-(defun redditting-get-kind (json)
+(defconst redditting-url "http://www.reddit.com")
+(defconst redditting-account-info-url (concat redditting-url "/api/me.json"))
+(defconst redditting-login-url (concat redditting-url "/api/login"))
+
+(defun redditting-kind (json)
   (cdr (assq 'kind json)))
 
-(defun redditting-is-kind (json kind)
-  (string= (redditting-get-kind json) kind))
+(defun redditting-kindp (json kind)
+  (string= (redditting-kind json) kind))
 
+;; subreddit
 (defun redditting-subreddit-url (subreddit &optional order)
-  (let* ((main-url (concat redditting-url "/r/" subreddit "/")))
+  (let ((main-url (concat redditting-url "/r/" subreddit "/")))
     (concat main-url order ".json")))
 
-(defun redditting-get-subreddit (subreddit &optional order)
-  (let ((buffer (url-retrieve-synchronously (redditting-subreddit-url subreddit order))))
-    (set-buffer buffer)
-    (json-read-from-string (substring (buffer-string) (string-match "^$" (buffer-string))))))
-
-(defun redditting-get-subreddit-property (subreddit property)
-  (let ((subreddit (redditting-get-subreddit subreddit)))
+(defun redditting-json (url)
+  (set-buffer (url-retrieve-synchronously url))
+  (json-read-from-string (substring (buffer-string) (string-match "^$" (buffer-string)))))
+  
+(defun redditting-subreddit-property (subreddit property)
+  (let ((subreddit (redditting-json (redditting-subreddit-url subreddit))))
     (cdr (assq property (cdr (assq 'data subreddit))))))
 
-(redditting-get-subreddit-property "programming" 'children)
-
-(defun redditting-get-subreddit-listing-property (subreddit property)
-  (let ((listing (redditting-get-subreddit-property subreddit 'children)))
+(defun redditting-subreddit-listing-property (subreddit property)
+  (let ((listing (redditting-subreddit-property subreddit 'children)))
     (mapcar (lambda (elem) (cdr (assq property (cdr (assq 'data elem)))))
 	    listing)))
 
+;; comments
+(defun redditting-subreddit-comments-url (subreddit article &optional sort)
+  (let ((main-url (concat redditting-url "/r/" subreddit "/comments/")))
+    (concat main-url article ".json?sort=" sort)))
+
+(defun redditting-article-comments (subreddit article &optional sort)
+  (let ((comments (redditting-json (redditting-subreddit-comments-url subreddit article sort))))
+    comments))
+    
 ;; tests
-(redditting-get-subreddit-listing-property "programming" 'id)
-(redditting-get-subreddit-listing-property "programming" 'title)
-(redditting-get-subreddit-listing-property "programming" 'num_comments)
-(redditting-get-subreddit-listing-property "programming" 'ups)
-(redditting-get-subreddit-listing-property "programming" 'downs)
-(redditting-get-subreddit-listing-property "programming" 'score)
+(redditting-article-comments "programming" "20qojw")
+(redditting-subreddit-listing-property "programming" 'id)
+(redditting-subreddit-listing-property "programming" 'title)
+(redditting-subreddit-listing-property "programming" 'num_comments)
+(redditting-subreddit-listing-property "programming" 'ups)
+(redditting-subreddit-listing-property "programming" 'downs)
+(redditting-subreddit-listing-property "programming" 'score)
+
+(defun redditting-display-subreddit (&optional subreddit)
+  (let* ((subreddit (if subreddit subreddit "all"))
+	 (titles (redditting-subreddit-listing-property subreddit 'title))
+	 (scores (redditting-subreddit-listing-property subreddit 'score)))
+    (set-buffer "*redditting-mode*")
+    (mapc #'insert (mapcar (lambda (title) (concat title "\n")) titles))
+    (mapc #'insert (mapcar (lambda (score) (concat (number-to-string score) "\n")) scores))))
 
 (defun redditting-mode ()
-  (error "work in progress"))
+  (interactive)
+  (with-current-buffer (get-buffer-create "*redditting-mode*")
+    (switch-to-buffer-other-window "*redditting-mode*")
+    (read-only-mode -1)
+    (erase-buffer)
+    (redditting-display-subreddit)
+    (read-only-mode 1)))
 
 (provide 'redditting-mode)
 
 ;;; redditting-mode.el ends here
-
